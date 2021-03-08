a6788d2ce231de398a41c01842d8a3599452f478
MuneshDevnani <muneshdevnani@gmail.com>
2021-03-08 20:22:05 +0500

    login signup

---
+++ client/src/pages/login.tsx
@@ -0,0 +1,63 @@
+import Axios from 'axios';
+import { useRouter } from 'next/dist/client/router';
+import Head from 'next/head'
+import Link from "next/link";
+import { FormEvent, useState } from 'react';
+import InputGroup from '../components/InputGroup'
+
+export default function Home() {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [errors, setErrors] = useState<any>({})
+
+  const router = useRouter()
+  
+  const submitForm = async (event: FormEvent) => {
+    event.preventDefault()
+    try {
+      const res = await Axios.post('/auth/login', {
+        password, username
+      })
+      router.push('/')
+    } catch (err) {
+      console.log(err);
+      setErrors(err.response.data)
+    }
+  }
+  return (
+    <div className="flex">
+      <Head>
+        <title>Log in</title>
+      </Head>
+
+      <div className="w-40 h-screen bg-center bg-cover" style={{ backgroundImage: "url('images/tiles.jpg')" }}></div>
+      <div className="flex flex-col justify-center pl-6">
+        <div className="w-70">
+          <h1 className="mb-2 text-lg font-medium">Sign Up</h1>
+          <p className="mb-10 text-xs">By continuing, you agree to our User Agreement and Privacy Policy.</p>
+          <form onSubmit= {submitForm}>
+            <div className="mb-6">
+              <label htmlFor="agreement" className="text-xs cursor-pointer">
+                I agree to get emails about cool stuff on Reddit
+            </label>
+            </div>
+             <InputGroup className="mb-2" type="text" value={username} setValue={setUsername} 
+            placeholder= "USERNAME" error={errors.username} />
+             <InputGroup className="mb-2" type="password" value={password} setValue={setPassword} 
+            placeholder= "PASSWORD" error={errors.password} />
+           
+            <button className="w-full py-2 mb-4 text-xs font-bold text-white uppercase bg-blue-500 border border-blue-500 rounded">
+              Login
+            </button>
+          </form>
+          <small>
+            New to Readit?
+          <Link href="/register">
+              <a className="ml-1 text-blue-500 uppercase">sign Up</a>
+            </Link>
+          </small>
+        </div>
+      </div>
+    </div>
+  )
+}
