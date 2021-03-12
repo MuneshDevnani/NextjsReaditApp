0807cb959fba90c32f8202f29a278fb0d4b4b472
MuneshDevnani <muneshdevnani@gmail.com>
2021-03-12 20:20:40 +0500

    submit post commited

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -38,19 +38,18 @@ export default function Home() {
             </div>
             <div>
               {topSubs?.map((sub: Sub) =>(
-                <div key={sub.name} className="flex items-center px-4 py-2 text-xs border-b">
-                 <div className="mr-2 overflow-hidden rounded-full cursor-pointer">
+                <div key={sub.name} className="flex items-center px-4 py-2 text-xs border-b">     
                  <Link href={`/r/${sub.name}`}>
                   <Image
                   src={sub.imageUrl}
                   alt="Sub"
+                  className="rounded-full cursor-pointer"
                   width={6 * 16 / 4}
                   height={6 * 16 / 4}
                   />
                   </Link>
-                 </div>
                   <Link href={`/r/${sub.name}`}>
-                    <a className="font-bold hover:cursor-pointer">
+                    <a className="ml-2 font-bold hover:cursor-pointer">
                       /r/{sub.name}
                     </a>
                   </Link>
