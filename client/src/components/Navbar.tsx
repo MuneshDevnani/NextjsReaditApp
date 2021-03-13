4e28f82141cdcfa41fecd8d9a4a5520b06666f61
MuneshDevnani <muneshdevnani@gmail.com>
2021-03-13 17:40:33 +0500

    search subname create community responsive

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,13 +1,22 @@
 import Link from 'next/link'
 import RedditLogo from '../images/logo.svg'
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
 
 import { useAuthDispatch, useAuthState } from "../context/auth";
 import Axios from 'axios';
+import { Sub } from '../types';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 const Navbar: React.FC = () => {
+  const [name, setName] = useState('')
+  const [subs, setSubs] = useState<Sub[]>([])
+  const [timer, setTimer] = useState(null)
+
   const { authenticated, loading } = useAuthState()
   const dispatch = useAuthDispatch()
+
+  const router= useRouter()
 
   const logout = () => {
     Axios.get('/auth/logout')
@@ -19,49 +28,110 @@ const Navbar: React.FC = () => {
       )
   }
 
-  return <div className="fixed inset-x-0 top-0 z-10 flex items-center justify-center h-12 px-5 bg-white">
+  useEffect(() => {
+    if(name.trim() === ''){
+      setSubs([])
+      return
+    }
+    searchSubs()
+  }, [name])
+
+  const searchSubs = async () => {
+    clearTimeout(timer)
+    setTimer(setTimeout(async () => {
+      try {
+        const {data} = await Axios.get(`/subs/search/${name}`)
+        setSubs(data)
+        console.log(data);
+        
+      } catch (err) {
+        console.log(err);
+      }
+    }, 250))
+  }
+
+  const goToSub = (subName: string) => {
+    router.push(`/r/${subName}`)
+    setName('')
+  }
+  return (
+  <div className="fixed inset-x-0 top-0 z-10 flex items-center justify-between h-12 px-5 bg-white">
     <div className="flex items-center">
       <Link href="/">
+        <a>
         <RedditLogo className="w-8 h-8 mr-2" />
+        </a>
       </Link>
-      <span className="text-2xl font-semibold">
+      <span className="hidden text-2xl font-semibold lg:block">
         <Link href="/">readit</Link>
       </span>
     </div>
+
+    {/* serach input */}
+    <div className="max-w-full px-4 w-160 ">
     <div
-      className="flex items-center mx-auto bg-gray-100 border rounded hover:border-blue-500 hover:bg-white"
+      className="relative flex items-center bg-gray-100 border rounded hover:border-blue-500 hover:bg-white"
     >
       <i className="pl-4 pr-3 text-gray-500 fas fa-search"></i>
       <input
         placeholder="search"
         type="text"
-        className="py-1 pr-3 bg-transparent rounded w-160 focus:outline-none"
+        className="py-1 pr-3 bg-transparent rounded focus:outline-none"
+        value={name}
+        onChange= {e => setName(e.target.value)}
       />
+      <div className="absolute left-0 right-0 bg-white" style={{top: '100%'}}>
+        {subs?.map((sub) => (
+          <div 
+          className="flex items-center px-4 py-3 cursor-pointer hover:bg-gray-200"
+          onClick={() => goToSub(sub.name)}
+          >
+              <Image
+              src={sub.imageUrl}
+              alt="Sub"
+              className="rounded-full"
+              height ={(8 * 16) /4}
+              width ={(8 * 16) /4} 
+              />
+              <div className="ml-4 text-sm">
+                <p className="font-medium">{sub.name}</p>
+                <p className="text-gray-600">{sub.title}</p>
+              </div>
+          </div>
+        ))}
+      </div>
     </div>
-    <div className="felx">
-      {!loading && (authenticated ? (
-        <button
-          className="w-32 py-1 mr-4 leading-5 hollow blue button"
-          onClick={logout}
-        >Logout</button>
-      ) : (
-        <Fragment>
-          <Link href="/login">
-            <a className="w-32 py-1 mr-4 leading-5 hollow blue button">
-              Login
-          </a>
-          </Link>
-          <Link href="/register">
-            <a className="w-32 py-1 leading-5 blue button">
-              Sign up
-          </a>
-          </Link>
-        </Fragment>
-      ))}
-
     </div>
+    {/* Auth Buttons */}
+    <div className="flex">
+        {!loading &&
+          (authenticated ? (
+            // Show logout
+            <button
+              className="hidden w-20 py-1 mr-4 leading-5 sm:block lg:w-32 hollow blue button"
+              onClick={logout}
+            >
+              Logout
+            </button>
+          ) : (
+            <Fragment>
+              <Link href="/login">
+                <a className="hidden w-20 py-1 mr-4 leading-5 sm:block lg:w-32 hollow blue button">
+                  log in
+                </a>
+              </Link>
+              <Link href="/register">
+                <a className="hidden w-20 py-1 leading-5 sm:block lg:w-32 blue button">
+                  sign up
+                </a>
+              </Link>
+            </Fragment>
+          ))}
+      </div>
   </div>
+  )
 }
+  
 
 
 export default Navbar
--- src/server.ts
+++ src/server.ts
@@ -12,6 +12,7 @@ import authRoutes from './routes/auth'
 import postsRoutes from './routes/posts'
 import subRoutes from './routes/subs'
 import miscRoutes from './routes/misc'
+import userRoutes from './routes/users'
 
 import trim from './middleware/trim'
 
@@ -36,6 +37,7 @@ app.use('/api/auth', authRoutes)
 app.use('/api/posts', postsRoutes)
 app.use('/api/subs', subRoutes)
 app.use('/api/misc', miscRoutes)
+app.use('/api/users', userRoutes)
 
 app.listen(port, async () =>{
     console.log(`Server running at http:localhost:${port}`);
